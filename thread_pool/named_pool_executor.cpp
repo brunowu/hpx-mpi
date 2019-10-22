ac0559a00b7b7879959fb9955ddbc25cb466b611
wu7 <xin.wu@fz-juelich.de>
2019-10-22 15:17:07 +0200

    add hpx+mpi+thread pool

---
+++ thread_pool/named_pool_executor.cpp
@@ -0,0 +1,124 @@
+#include <hpx/hpx_init.hpp>
+#include <hpx/include/async.hpp>
+#include <hpx/lcos/when_all.hpp>
+#include <hpx/include/resource_partitioner.hpp>
+#include <hpx/include/parallel_execution.hpp>
+#include <hpx/runtime/threads/executors/pool_executor.hpp>
+#include <hpx/include/parallel_for_loop.hpp>
+#include <hpx/include/threads.hpp>
+#include <hpx/util/lightweight_test.hpp>
+
+#include <cstddef>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include <mpi.h>
+
+const int max_threads = 4;
+const int mpi_threads = 1;
+
+// dummy function we will call using async
+void dummy_task(std::size_t n, std::string text)
+{
+    for (std::size_t i(0); i < n; ++i)
+    {
+        std::cout << hpx::get_os_thread_count() << " : " << text << " iteration " << i << " . ";
+    }
+
+    std::cout << std::endl;
+}
+
+int hpx_main(int argc, char* argv[])
+{
+
+    HPX_TEST_EQ(std::size_t(4), hpx::resource::get_num_threads());
+    HPX_TEST_EQ(std::size_t(2), hpx::resource::get_num_thread_pools());
+    HPX_TEST_EQ(std::size_t(0), hpx::resource::get_pool_index("default"));
+    HPX_TEST_EQ(std::size_t(1), hpx::resource::get_pool_index("mpi"));
+
+    hpx::threads::scheduled_executor exec_0_hp =
+        hpx::threads::executors::pool_executor("default",
+        hpx::threads::thread_priority_high);
+
+    hpx::threads::scheduled_executor exec_0 =
+        hpx::threads::executors::pool_executor("default",
+        hpx::threads::thread_priority_default);
+
+   std::vector<hpx::future<void>> exec_futures;
+
+   exec_futures.push_back(hpx::async(exec_0_hp, &dummy_task, 2, "HP default"));
+
+   exec_futures.push_back(hpx::async(exec_0, &dummy_task, 3, "NM default"));
+
+   hpx::threads::scheduled_executor execs;
+        
+   hpx::threads::scheduled_executor execs_hp;
+
+   execs = hpx::threads::executors::pool_executor("mpi", hpx::threads::thread_priority_default);
+
+   execs_hp = hpx::threads::executors::pool_executor("mpi", hpx::threads::thread_priority_high);
+
+   exec_futures.push_back(hpx::async(execs, &dummy_task, 1, "HP MPI"));
+
+   exec_futures.push_back(hpx::async(execs_hp, &dummy_task, 4, "NM MPI"));
+
+   // check that the default executor still works
+   hpx::parallel::execution::default_executor large_stack_executor(
+        hpx::threads::thread_stacksize_large);
+
+   exec_futures.push_back(
+        hpx::async(large_stack_executor, &dummy_task, 3, "true default + large stack")
+   );
+
+    // just wait until everything is done
+   hpx::when_all(exec_futures).get();
+
+   return hpx::finalize();
+}
+
+// this test must be run with 4 threads
+int main(int argc, char* argv[])
+{
+    std::vector<std::string> cfg = {
+        "hpx.os_threads=" + std::to_string(max_threads)
+    };
+
+    // create the resource partitioner
+    hpx::resource::partitioner rp(argc, argv, std::move(cfg));
+
+    // before adding pools - set the default pool name to "pool:0"
+    rp.set_default_pool_name("default");
+
+    std::string mpi_pool_name = "mpi";
+
+    rp.create_thread_pool(mpi_pool_name,
+            hpx::resource::scheduling_policy::local_priority_fifo);
+
+    int thread_count = 0;
+
+    for (const hpx::resource::numa_domain& d : rp.numa_domains()){
+	for (const hpx::resource::core& c : d.cores()){
+	    for (const hpx::resource::pu& p : c.pus()){
+		if(thread_count < mpi_threads){
+		    std::cout << "Added pu " << thread_count
+                              << " to " << mpi_pool_name << "\n";
+		    rp.add_resource(p, mpi_pool_name);
+                    thread_count++;
+		}
+	    }
+	}
+    }
+
+    int initialized;
+
+    MPI_Initialized(&initialized);
+
+    if(!initialized){
+        MPI_Init(&argc, &argv);
+    }
+    // now run the test
+    HPX_TEST_EQ(hpx::init(), 0);
+    return hpx::util::report_errors();
+}
+
